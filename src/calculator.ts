b4c370f2b822ee4fde7cb5e89d0f835af25e68ee
Milan Bagiya <92111219+MilanBagiya@users.noreply.github.com>
2025-03-12 16:43:47 +0000

    Feat: Implement custom delimiter support

--- src/calculator.ts
+++ src/calculator.ts
@@ -1,13 +1,20 @@
 export class Calculator {
-
     add(numbers: string): number {
         if (!numbers) return 0;
 
-        const normalizedString: string = numbers.replace(/\n/g, ",");
+        let delimiter = ",";
+        let numberString = numbers;
 
-        const numArray: number[] = normalizedString.split(',').map((num: string) => parseInt(num, 10));
+        if (numbers.startsWith("//")) {
+            const parts = numbers.split("\n", 2);
+            delimiter = parts[0].substring(2);
+            numberString = parts[1];
+        }
+
+        const normalizedString: string = numberString.replace(/\n/g, delimiter);
+
+        const numArray: number[] = normalizedString.split(delimiter).map((num: string) => parseInt(num, 10));
 
         return numArray.reduce((sum, num) => sum + num, 0);
     }
-
 }
