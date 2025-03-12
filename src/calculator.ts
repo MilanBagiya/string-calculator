6b488ca88c673019f6c3d4de407ce4581896daf0
Milan Bagiya <92111219+MilanBagiya@users.noreply.github.com>
2025-03-12 16:21:56 +0000

    Feat: Handle comma-separated numbers sum

--- src/calculator.ts
+++ src/calculator.ts
@@ -2,8 +2,10 @@ export class Calculator {
 
     add(numbers: string): number {
         if (!numbers) return 0;
-        
-        return parseInt(numbers, 10);
+
+        const numArray: number[] = numbers.split(',').map((num: string) => parseInt(num, 10));
+
+        return numArray.reduce((sum, num) => sum + num, 0);
     }
 
 }
