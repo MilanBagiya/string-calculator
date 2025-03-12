d1dbd59df6d2e0a6f4ebf80461536b2fcb4c9dae
Milan Bagiya <92111219+MilanBagiya@users.noreply.github.com>
2025-03-12 16:37:29 +0000

    Feat: Implement new line delimiter support

--- src/calculator.ts
+++ src/calculator.ts
@@ -3,7 +3,9 @@ export class Calculator {
     add(numbers: string): number {
         if (!numbers) return 0;
 
-        const numArray: number[] = numbers.split(',').map((num: string) => parseInt(num, 10));
+        const normalizedString: string = numbers.replace(/\n/g, ",");
+
+        const numArray: number[] = normalizedString.split(',').map((num: string) => parseInt(num, 10));
 
         return numArray.reduce((sum, num) => sum + num, 0);
     }
